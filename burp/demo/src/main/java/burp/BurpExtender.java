e5fbe9dbab6cf55230c036bd83d2f5c55e9bd833
peigen <peigen@qianxin.com>
2022-02-11 20:12:36 +0800

    xss Intruder

--- burp/demo/src/main/java/burp/BurpExtender.java
+++ burp/demo/src/main/java/burp/BurpExtender.java
@@ -2,28 +2,111 @@ package burp;
 
 import java.io.PrintWriter;
 
-public class BurpExtender implements IBurpExtender {
+public class BurpExtender implements IBurpExtender,IIntruderPayloadGeneratorFactory,IIntruderPayloadProcessor {
+    private IExtensionHelpers helpers;
 
+    // hard-coded payloads
+    // [in reality, you would use an extension for something cleverer than this]
+    private static final byte[][] PAYLOADS =
+            {
+                    "|".getBytes(),
+                    "<script>alert(1)</script>".getBytes(),
+            };
+
+    //
+    // implement IBurpExtender
+    //
 
     @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
+    {
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
+
         // set our extension name
-        callbacks.setExtensionName("Hello world extension");
+        callbacks.setExtensionName("Custom intruder payloads");
 
-        // obtain our output and error streams
-        PrintWriter stdout = new PrintWriter(callbacks.getStdout(), true);
-        PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
+        // register ourselves as an Intruder payload generator
+        callbacks.registerIntruderPayloadGeneratorFactory(this);
 
-        // write a message to our output stream
-        stdout.println("Hello output");
+        // register ourselves as an Intruder payload processor
+        callbacks.registerIntruderPayloadProcessor(this);
+    }
 
-        // write a message to our error stream
-        stderr.println("Hello errors");
+    //
+    // implement IIntruderPayloadGeneratorFactory
+    //
 
-        // write a message to the Burp alerts tab
-        callbacks.issueAlert("Hello alerts");
+    @Override
+    public String getGeneratorName()
+    {
+        return "My custom payloads";
+    }
 
-        // throw an exception that will appear in our error stream
-        throw new RuntimeException("Hello exceptions");
+    @Override
+    public IIntruderPayloadGenerator createNewInstance(IIntruderAttack attack)
+    {
+        // return a new IIntruderPayloadGenerator to generate payloads for this attack
+        return new IntruderPayloadGenerator();
+    }
+
+    //
+    // implement IIntruderPayloadProcessor
+    //
+
+    @Override
+    public String getProcessorName()
+    {
+        return "Serialized input wrapper";
+    }
+
+    @Override
+    public byte[] processPayload(byte[] currentPayload, byte[] originalPayload, byte[] baseValue)
+    {
+        // decode the base value
+        String dataParameter = helpers.bytesToString(helpers.base64Decode(helpers.urlDecode(baseValue)));
+
+        // parse the location of the input string in the decoded data
+        int start = dataParameter.indexOf("input=") + 6;
+        if (start == -1)
+            return currentPayload;
+        String prefix = dataParameter.substring(0, start);
+        int end = dataParameter.indexOf("&", start);
+        if (end == -1)
+            end = dataParameter.length();
+        String suffix = dataParameter.substring(end, dataParameter.length());
+
+        // rebuild the serialized data with the new payload
+        dataParameter = prefix + helpers.bytesToString(currentPayload) + suffix;
+        return helpers.stringToBytes(helpers.urlEncode(helpers.base64Encode(dataParameter)));
+    }
+
+    //
+    // class to generate payloads from a simple list
+    //
+
+    class IntruderPayloadGenerator implements IIntruderPayloadGenerator
+    {
+        int payloadIndex;
+
+        @Override
+        public boolean hasMorePayloads()
+        {
+            return payloadIndex < PAYLOADS.length;
+        }
+
+        @Override
+        public byte[] getNextPayload(byte[] baseValue)
+        {
+            byte[] payload = PAYLOADS[payloadIndex];
+            payloadIndex++;
+            return payload;
+        }
+
+        @Override
+        public void reset()
+        {
+            payloadIndex = 0;
+        }
     }
 }
