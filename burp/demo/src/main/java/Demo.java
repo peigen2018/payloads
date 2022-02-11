d1ee5739b5f0e306785551fa900cfe8cb9bd99f8
peigen <peigen@qianxin.com>
2022-02-11 10:18:28 +0800

    demo without build script

---
+++ burp/demo/src/main/java/Demo.java
@@ -0,0 +1,30 @@
+import burp.IBurpExtender;
+import burp.IBurpExtenderCallbacks;
+
+import java.io.PrintWriter;
+
+public class Demo implements IBurpExtender {
+
+
+    @Override
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
+        // set our extension name
+        callbacks.setExtensionName("Hello world extension");
+
+        // obtain our output and error streams
+        PrintWriter stdout = new PrintWriter(callbacks.getStdout(), true);
+        PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
+
+        // write a message to our output stream
+        stdout.println("Hello output");
+
+        // write a message to our error stream
+        stderr.println("Hello errors");
+
+        // write a message to the Burp alerts tab
+        callbacks.issueAlert("Hello alerts");
+
+        // throw an exception that will appear in our error stream
+        throw new RuntimeException("Hello exceptions");
+    }
+}
